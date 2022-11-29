832a50b69d0b2c12e78c002dfde7ed086f15098b
Mert Ali Özçelik <bakruux@gmail.com>
2022-11-30 02:49:53 +0300

    MyList

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Mert");
+            Console.WriteLine("Hello World");
+
+
+
+        }
+    }
+
+}
